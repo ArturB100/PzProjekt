80e9f650b33198253512c0663b333cf154de6316
Artur Bochenek <artur119@vp.pl>
2024-09-02 20:39:50 +0200

    added plugin implementation to repository

---
+++ NeuralNetworkClient/AIaction.cs
@@ -0,0 +1,74 @@
+﻿using PzProjekt;
+using PzProjekt.fight;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkClient
+{
+    public class AIaction : IBotAction
+    {
+
+        public string NameOfPlugin()
+        {
+            return "neural network";
+        }
+
+        public async Task MakeMove(Fight fight)
+        {
+            int action = await NeuralNetworkClient.GetPredictionAsync(
+                fight.DistanceBetweenCharacters,
+                fight.Enemy.Parameters.ActualStamina,
+                fight.Enemy.Parameters.MaxStamina,
+                fight.Enemy.Parameters.ActualHP,
+                fight.Enemy.Parameters.MaxHP,
+                fight.Enemy.BaseStatistics.Stamina,
+                fight.Enemy.BaseStatistics.Magica,
+                fight.Enemy.BaseStatistics.Vitality,A
+                fight.Enemy.BaseStatistics.Charisma,
+                fight.Enemy.BaseStatistics.Agility,
+                fight.Enemy.BaseStatistics.Attack,
+                fight.Enemy.BaseStatistics.Defence,
+                fight.Player.Parameters.ActualStamina,
+                fight.Player.Parameters.MaxStamina,
+                fight.Player.Parameters.ActualHP,
+                fight.Player.Parameters.MaxHP
+            );
+            CharacterFightActions characterFightActions = fight.CharacterFightActions;
+
+            Debug.WriteLine("neural network response " +  action );
+            switch (action)
+            {
+                case 1:
+                    characterFightActions.Attack(AttackType.STRONG);
+                    break;
+                case 2:
+                    characterFightActions.Attack(AttackType.MEDIUM);
+                    break;
+                case 3:
+                    characterFightActions.Attack(AttackType.WEAK);
+                    break;
+                case 4:
+                    characterFightActions.MoveTowardsEnemy();
+                    break;
+                case 5:
+                    characterFightActions.MoveFromEnemy();
+                    break;
+                case 6:
+                    characterFightActions.Sleep();
+                    break;
+                case 7:
+                    characterFightActions.SatisfyTheCrowd();
+                    break;
+                default:
+                    characterFightActions.Sleep();
+                    break;
+
+            }
+        }
+    }
+}
---
+++ NeuralNetworkClient/NeuralNetworkClient.cs
@@ -0,0 +1,73 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+namespace NeuralNetworkClient
+{
+    public class NeuralNetworkClient
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public static async Task<int> GetPredictionAsync(
+            double distanceBetweenCharacters,
+            double playerActualStamina,
+            double playerMaxStamina,
+            double playerActualHP,
+            double playerMaxHP,
+            double playerBaseStamina,
+            double playerBaseMagica,
+            double playerBaseVitality,
+            double playerBaseCharisma,
+            double playerBaseAgility,
+            double playerBaseAttack,
+            double playerBaseDefence,
+            double enemyActualStamina,
+            double enemyMaxStamina,
+            double enemyActualHP,
+            double enemyMaxHP)
+        {
+            var data = new
+            {
+                DistanceBetweenCharacters = distanceBetweenCharacters,
+                PlayerActualStamina = playerActualStamina,
+                PlayerMaxStamina = playerMaxStamina,
+                PlayerActualHP = playerActualHP,
+                PlayerMaxHP = playerMaxHP,
+                PlayerBaseStamina = playerBaseStamina,
+                PlayerBaseMagica = playerBaseMagica,
+                PlayerBaseVitality = playerBaseVitality,
+                PlayerBaseCharisma = playerBaseCharisma,
+                PlayerBaseAgility = playerBaseAgility,
+                PlayerBaseAttack = playerBaseAttack,
+                PlayerBaseDefence = playerBaseDefence,
+                EnemyActualStamina = enemyActualStamina,
+                EnemyMaxStamina = enemyMaxStamina,
+                EnemyActualHP = enemyActualHP,
+                EnemyMaxHP = enemyMaxHP
+            };
+
+            string jsonData = JsonConvert.SerializeObject(data);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            try
+            {
+                HttpResponseMessage response = await client.PostAsync("http://127.0.0.1:5000/predict", content);
+
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                var result = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                int action = result.action;
+
+                return action;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine("Request error: " + e.Message);
+                throw;
+            }
+        }
+
+    }
+}
